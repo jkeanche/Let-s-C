961603b01d62ce97c3a28ec44470273a61b053e0
codepal <jkeanche@gmail.com>
2022-12-04 14:06:09 +0300

    Hello-world: our first c program

---
+++ hello-world/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/** Classical Hello World programDisplays 'Hello, World!'
+*/
+int main() {
+   printf("Hello, World!"); // use in-built printf function to print to console
+   return 0; // 0 signifies program executed successfully
+}
